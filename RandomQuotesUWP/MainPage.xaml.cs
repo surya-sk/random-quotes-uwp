c9c44dcafe748b60fe7405b1818b60beb2e45bbe
surya <surya.sk05@outlook.com>
2020-08-09 11:41:36 +0530

    v 1.0.0.

--- RandomQuotesUWP/MainPage.xaml.cs
+++ RandomQuotesUWP/MainPage.xaml.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -12,6 +17,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Notifications;
+using Microsoft.Toolkit.Uwp.Notifications;
+using Windows.UI.Composition;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +30,222 @@ namespace RandomQuotesUWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Quotes quotes = new Quotes();
+        HttpClient client;
+
+        string url = "https://yourrandomquotes.herokuapp.com/quote";//tp://localhost:8081/quote";
+
         public MainPage()
         {
             this.InitializeComponent();
+            client = new HttpClient();
+
+            client.BaseAddress = new Uri(url);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            LoadQuote();
+            UpdateLiveTile();
         }
+
+
+
+
+        //private async Task UpdateQuote()
+        //{
+        //    //bool done = false;
+
+        //    //while (!done)
+        //    //{
+        //    //    done = await DeserializeJson();
+        //    //    Quote.Text = quotes.quote;
+        //    //    Author.Text = "-" + quotes.by;
+        //    //    if (quotes.quote.Length > 20)
+        //    //    {
+        //    //        Quote.TextWrapping = TextWrapping.Wrap;
+        //    //    }
+        //    //}
+        //}
+
+        /// <summary>
+        /// Returns the json string from the url
+        /// </summary>
+        /// <returns>the string in JSON format</returns>
+        public async Task<Quotes> GetStringFromURL()
+        {
+            //string url = "https://yourrandomquotes.herokuapp.com/quote";
+            //ttpClient client = new HttpClient();
+
+            //client.BaseAddress = new Uri(url);
+            //client.DefaultRequestHeaders.Accept.Clear();
+            //client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            System.Net.Http.HttpResponseMessage result = await client.GetAsync(url).ConfigureAwait(false);
+            if (result.IsSuccessStatusCode)
+            {
+                string jsonString = await result.Content.ReadAsStringAsync().ConfigureAwait(false);
+                quotes = JsonConvert.DeserializeObject<Quotes>(jsonString);
+
+                //Quote.Text = quotes.quote;
+                //Author.Text = quotes.by;
+
+            }
+
+
+
+
+            return quotes;
+
+        }
+
+        async Task<Quotes> GetResponse()
+        {
+
+            quotes = await GetStringFromURL().ConfigureAwait(false);
+
+            return quotes;
+        }
+
+        /// <summary>
+        /// Parsing the JSON file
+        /// </summary>
+        //private async Task<bool> DeserializeJson()
+        //{
+        //    bool done = false;
+        //    bool status = false;
+
+        //    while (!done)
+        //    {
+        //        quotes = JsonConvert.DeserializeObject<Quotes>(GetStringFromURL());
+        //        status = quotes != null;
+        //        await Task.Yield();
+        //        return status;
+        //    }
+
+        //    return status;
+        //}
+
+
+
+        /// <summary>
+        /// A class that helps in parsing the JSON strings to simplify access in other methods
+        /// </summary>
+        public class Quotes
+        {
+            public string quote { get; set; }
+            public string by { get; set; }
+        }
+        /// <summary>
+        /// When clicked, gets a new quote
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NewQuote_Click(object sender, RoutedEventArgs e)
+        {
+            LoadQuote();
+            UpdateLiveTile();
+        }
+
+
+
+
+        /// <summary>
+        /// Displays a new quote
+        /// </summary>
+        private void LoadQuote()
+        {
+            quotes = GetResponse().Result;
+
+            Quote.Text = quotes.quote;
+            Author.Text = quotes.by;
+
+            if (quotes.quote.Length < 300)
+            {
+                Quote.TextWrapping = TextWrapping.Wrap;
+            }
+        }
+
+        /// <summary>
+        /// Reads the quote aloud
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void Read_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement mediaElement = new MediaElement();
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync(quotes.quote);
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
+        }
+
+        /// <summary>
+        /// Updates the live tile to show the current quote
+        /// </summary>
+        private void UpdateLiveTile()
+        {
+            var tileContent = new TileContent()
+            {
+                Visual = new TileVisual()
+                {
+
+                    TileMedium = new TileBinding()
+                    {
+                        Branding = TileBranding.Name,
+                        DisplayName = "Random Quotes",
+                        Content = new TileBindingContentAdaptive()
+                        {
+                            Children =
+                {
+                    new AdaptiveText()
+                    {
+                        Text = "Quote By",
+                        HintWrap = true,
+                        HintMaxLines = 2
+                    },
+                    new AdaptiveText()
+                    {
+                        Text = quotes.by,
+                        HintStyle = AdaptiveTextStyle.CaptionSubtle
+                    }
+                }
+                        }
+                    },
+                    TileWide = new TileBinding()
+                    {
+                        Branding = TileBranding.NameAndLogo,
+                        DisplayName = "Random Quotes UWP",
+                        Content = new TileBindingContentAdaptive()
+                        {
+                            Children =
+                {
+                    new AdaptiveText()
+                    {
+                        Text = "Today's Quote"
+                    },
+                    new AdaptiveText()
+                    {
+                        Text = quotes.by,
+                        HintStyle = AdaptiveTextStyle.CaptionSubtle
+                    },
+                    new AdaptiveText()
+                    {
+                        Text = quotes.quote,
+                        HintStyle = AdaptiveTextStyle.CaptionSubtle,
+                        HintMinLines = 2,
+                        HintMaxLines = 4,
+                        HintWrap = true
+                    }
+                }
+                        }
+                    },
+                }
+            };
+
+            // Create the tile notification
+            var tileNotif = new TileNotification(tileContent.GetXml());
+
+            // And send the notification to the primary tile
+            TileUpdateManager.CreateTileUpdaterForApplication().Update(tileNotif);
+        }
+
     }
 }
